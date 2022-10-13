7696186c665ee78236c4164b8270b714b7b34b95
cocoball200 <katie@reware.co.kr>
2022-10-13 22:15:59 +0900

    feat: reservation, userReservation testing

--- base-concert-venue/__tests__/__mocks__/msw/handlers.js
+++ base-concert-venue/__tests__/__mocks__/msw/handlers.js
@@ -1,9 +1,20 @@
 import { rest } from "msw";
 import { readFakeData } from "@/__tests__/__mocks__/fakeData";
+import { fakeUserReservations } from "@/__tests__/__mocks__/fakeData/userReservations";
 
 export const handlers = [
   rest.get("http://localhost:3000/api/shows/:showId", async (req, res, ctx) => {
     const { fakeShows } = await readFakeData();
-    return res(ctx.json({ show: fakeShows[0] }));
+    const { showId } = req.params;
+
+    // index / showId = 0 has seats available in fake data
+    // index/ showid = 1 has No seats available
+    return res(ctx.json({ show: fakeShows[showId] }));
   }),
+  rest.get(
+    "http://localhost:3000/api/users/:userId/reservations",
+    async (req, res, ctx) => {
+      res(ctx.json({ userReservations: fakeUserReservations }));
+    }
+  ),
 ];
---
+++ base-concert-venue/__tests__/ui/reservation.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from "@testing-library/react";
+
+import { Reservation } from "@/components/reservations/Reservation";
+
+test("reservation page show correct number of availale seats", async () => {
+  render(<Reservation showId={0} submitPurchase={jest.fn()} />);
+
+  const seatCountText = await screen.findByText(/10 seats left/i);
+  expect(seatCountText).toBeInTheDocument();
+});
+
+test("reservation page shows sold out message ", async () => {
+  render(<Reservation showId={1} submitPurchase={jest.fn()} />);
+
+  const soldOutMessage = await screen.findByRole("heading", {
+    name: /sold out/i,
+  });
+  expect(soldOutMessage).toBeInTheDocument();
+
+  const purchaseButton = screen.getByRole("button", {
+    name: /purchase/i
+  });
+  expect(purchaseButton).not.toBeInTheDocument();
+}) 
